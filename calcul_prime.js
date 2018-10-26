39ee58d6d4577a86560b47d7470d260029c355d3
Sergiu0693 <44505208+Sergiu0693@users.noreply.github.com>
2018-10-26 19:35:10 +0300

    Add files via upload

---
+++ calcul_prime.js
@@ -0,0 +1,12 @@
+function is_prime(n)
+{
+for (var i = 2, i*i <=n; i++)
+if(n % i == 0)
+return false;
+  return true;
+}
+for (var i = 1e11; + < 1e12; i++)
+ if(is_prine(i))
+postMessage(i);
+
+postMessage("done"); 
